5acda4c4bedfee80e3b7df41120b09120f6d62bf
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-10 00:55:31 +0100

    Exercise 3

---
+++ programs/exercise2.rb
@@ -0,0 +1,9 @@
+#Comments exercise
+#Anything after the pound sign is a comment.
+
+puts "I could have code like this." # and the comment after is ignored
+
+#You can also use a comment to disable a piece of code
+# puts "This won't run"
+
+puts "This will run."
