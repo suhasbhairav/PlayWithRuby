674a89e9d1ecb8c68e5e6fddbbed5c3b611ea5e4
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-10 01:35:47 +0100

    Exercise 4

--- programs/exercise4.rb
+++ programs/exercise4.rb
@@ -1,23 +1,15 @@
-puts "I will now count my chickens"
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
 
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
-
-puts "Now I will count the eggs:"
-
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
-puts "Is it true that 3 + 2 < 5 - 7?"
-
-puts 3 + 2 < 5 - 7
-
-puts "What is 3+2? #{3+2}"
-puts "What is 5-7? #{5-7}"
-
-puts "Oh, that's why it's false."
-
-puts "How about some more."
-
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
