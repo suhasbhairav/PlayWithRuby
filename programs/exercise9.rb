a969c79a18b66978df46eee8dd74aa5883e2e1c5
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-10 16:43:25 +0100

    Exercise 9

---
+++ programs/exercise9.rb
@@ -0,0 +1,16 @@
+days = "Mon Tue Wed Thu Fri Sat"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts "..."
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want , or 5 or 6.
+"""
+
+
