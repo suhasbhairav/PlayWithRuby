e728b3a1fbe123eeb7c8eba0e25fea2b22673ddf
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-10 16:46:45 +0100

    Exercise 10

---
+++ programs/exercise10.rb
@@ -0,0 +1,12 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\a\\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat, persian_cat, backslash_cat, fat_cat
