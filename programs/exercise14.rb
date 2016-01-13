877ab0f0cb1de6d4765eb6213365501730d212a9
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-14 00:21:36 +0100

    Exercise 14

---
+++ programs/exercise14.rb
@@ -0,0 +1,23 @@
+user_name = ARGV.first # gets the firt argument
+prompt = '> '
+
+puts "Hi #{user_name}"
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}? "
+puts prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{user_name}? "
+puts prompt
+lives = $stdin.gets.chomp
+
+# a comma for puts is like using it twice
+puts "What kind of computer do you have? ", prompt
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure what it is.
+And you have a #{computer} computer.Nice.
+"""
+
